2637d0af2a02b58650004cf97f5823d8cadbabe2
Stuart Ellis <stuart@stuartellis.name>
2023-09-16 18:38:08 +0100

    fix: Enable click to work with PyInstaller

--- python/apps/project-spider/src/project_spider/cli/__init__.py
+++ python/apps/project-spider/src/project_spider/cli/__init__.py
@@ -1,6 +1,7 @@
 # SPDX-FileCopyrightText: 2023-present Stuart Ellis <stuart@stuartellis.name>
 #
 # SPDX-License-Identifier: MIT
+import sys
 import click
 
 from project_spider.__about__ import __version__
@@ -10,3 +11,6 @@ from project_spider.__about__ import __version__
 @click.version_option(version=__version__, prog_name="Project Spider")
 def project_spider():
     click.echo("Hello world!")
+
+if getattr(sys, 'frozen', False):
+    project_spider(sys.argv[1:])
